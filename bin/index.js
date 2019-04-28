d38417771cc42a6b03e6021ae125f14911a7867e
xiaoqiang-zhao <longze_xq@163.com>
2019-04-28 14:32:46 +0800

    修改目录结构

--- bin/index.js
+++ bin/index.js
@@ -6,13 +6,13 @@
 
 const program = require('commander');
 
-const setConfig = require('./src/set-config');
-const startUpload = require('./src/start');
-const reset = require('./src/reset');
-const getConfig = require('./src/get-config');
+const setConfig = require('../lib/set-config');
+const startUpload = require('../lib/start');
+const reset = require('../lib/reset');
+const getConfig = require('../lib/get-config');
 
 program
-  .version('1.0.0')
+  .version('1.0.1')
   // 配置相关参数
   .option('init, --init', '批量配置初始化参数，示例: upload-many-files init --server-url http://xx.xx.xx --folder-path /home/user/xxx --extname jpeg,png')
   .option('-su, --server-url <>', '上传路径配置，示例: upload-many-files --server-url http://xx.xx.xx')
