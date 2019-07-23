a54a988075141d58f2b815ddf5d3430c2f3c4dda
xiaoqiang-zhao <longze_xq@163.com>
2019-07-23 12:40:54 +0800

    添加 help 说明文字

--- bin/index.js
+++ bin/index.js
@@ -15,13 +15,13 @@ program
   .version(require('../package').version, '-v, --version')
   // 配置相关参数
   .option('init, --init', '批量配置初始化参数，示例: upload-many-files-init --server-url http://xx.xx.xx --folder-path /home/user/xxx --extname jpeg,png')
-  .option('-su, --server-url <>', '上传路径配置，示例: upload-many-files --server-url http://xx.xx.xx')
-  .option('-fp, --folder-path <>', '本地文件夹路径配置，示例: upload-many-files --folder-path /home/user/xxx')
-  .option('-s, --extname <>', '上传路径配置，示例: upload-many-files --extname jpeg,png')
+  .option('-su, --server-url <>', '上传路径配置(不可单独使用)，示例: upload-many-files --server-url http://xx.xx.xx')
+  .option('-fp, --folder-path <>', '本地文件夹路径配置(不可单独使用)，示例: upload-many-files --folder-path /home/user/xxx')
+  .option('-s, --extname <>', '上传路径配置(不可单独使用)，示例: upload-many-files --extname jpeg,png')
 
   // start 开始上传指令
-  .option('start, --start', '开始上传，示例: upload-many-files-start')
-  .option('upload-params, --upload-params <>', '上传额外参数，示例: upload-many-files-start --upload-params a=b,c=d')
+  .option('start, --start', '开始上传，示例: upload-many-files-start --upload-params a=b,c=d')
+  .option('upload-params, --upload-params <>', '上传额外参数(不可单独使用)，示例: upload-many-files-start --upload-params a=b,c=d')
 
   // 将设置回归到安装初始化状态
   .option('reset, --reset', '开始上传，示例: upload-many-files-reset')
